33a052d2efd58317c75e9c2eeb4c29a3d64e0199
Alexander <61645144+AlexDeveloper99@users.noreply.github.com>
2020-05-28 13:22:43 +0600

    first commit

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a, time, hour, min int
+	fmt.Scan(&a)
+	if a>=0 || a<360{
+		time = a*120
+		hour = time / 3600
+		min = time % 3600 / 60
+		fmt.Printf("It is %d hours %d minutes.",hour,min)
+	}
+}
+
+
+//
+//
+//package main
+//
+//import "fmt"
+//
+//func main() {
+//	var name int
+//
+//	fmt.Scan(&name)
+//	fmt.Println("It is",name/30,"hours", 2 * (name % 30),"minutes.")
+//}
